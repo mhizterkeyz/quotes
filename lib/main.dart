8a320639952bf6f547a63e9a70c7c72cce5833ee
Emmanuel Menyaga <sparklinsparky@gmail.com>
2022-11-27 10:55:36 +0100

    chore: move quote card to its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quotes/quote.dart';
+import 'package:quotes/quote_card.dart';
 
 void main() {
   runApp(const App());
@@ -47,34 +48,5 @@ class _QuoteListState extends State<QuoteList> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
         ));
-  }
-}
-
-class QuoteCard extends StatelessWidget {
-  const QuoteCard({
-    Key? key,
-    required this.quote,
-  }) : super(key: key);
-
-  final Quote quote;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Wrap(
-          direction: Axis.vertical,
-          spacing: 6.0,
-          children: [
-            Text(quote.text,
-                style: TextStyle(fontSize: 18, color: Colors.grey[600])),
-            Text(quote.author,
-                style: TextStyle(fontSize: 14, color: Colors.grey[800])),
-          ],
-        ),
-      ),
-    );
   }
 }
